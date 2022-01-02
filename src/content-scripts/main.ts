ab493e44314ade800844721258ccface4649107b
yto60 <mail@yto60.dev>
2022-01-02 17:59:07 +0900

    fix

--- src/content-scripts/main.ts
+++ src/content-scripts/main.ts
@@ -22,12 +22,15 @@ const wait = async (milliseconds: number) => {
   })
 }
 
-/** 要素の子孫を辿って、最初の葉を返す */
-const getFirstLeaf = ($parent: HTMLElement): HTMLElement => {
-  if ($parent.childElementCount > 0) {
-    return getFirstLeaf($parent.children[0] as HTMLElement)
+/** 要素の子孫を辿って、最初の contenteditable leaf を返す。見つからなければ null 。 */
+const getFirstContentEditableLeaf = ($el: HTMLElement): HTMLElement | null => {
+  if ($el.getAttribute('data-content-editable-leaf') === 'true') {
+    return $el
   }
-  return $parent
+  if ($el.childElementCount > 0) {
+    return getFirstContentEditableLeaf($el.children[0] as HTMLElement)
+  }
+  return null
 }
 
 const replaceTextInBlock = ($block: HTMLElement, replacedText: string) => {
@@ -56,9 +59,9 @@ const replaceText = async (searchVal: string, replaceTo: string) => {
 
   // ページ内の何かを click して focus してからでないと、 replaceText() で書き換えた内容が反映されない。
   // とりあえず notionTextBlocks の最初の要素を使う。(タイトルブロック等でも良い。)
-  const firstBlock = getFirstLeaf(notionTextBlocks[0])
-  firstBlock.click()
-  firstBlock.focus()
+  const firstBlock = getFirstContentEditableLeaf(notionTextBlocks[0])
+  firstBlock!.click()
+  firstBlock!.focus()
   await wait(500) // 少し待つ必要がある
 
   notionTextBlocks.forEach(($textBlock) => {
@@ -66,9 +69,17 @@ const replaceText = async (searchVal: string, replaceTo: string) => {
     if (text.includes(searchVal)) {
       // TODO 本当は毎回の置換前に確認したい
       const replacedText = text.replaceAll(searchVal, replaceTo)
-      const $leaf = getFirstLeaf($textBlock)
-      replaceTextInBlock($leaf, replacedText)
-      text = $textBlock.innerText
+      const $leaf = getFirstContentEditableLeaf($textBlock)
+      if ($leaf !== null) {
+        if ($leaf.childElementCount > 0) {
+          // data-content-editable-leaf=true な要素の内側に更に要素が存在する場合、文字がデコレーションされている。
+          // そのまま置換してしまうとデコレーションが消えるので、一旦何もせずにスキップする。
+          // TODO デコレーションを残したまま置換できるようにする
+        } else {
+          replaceTextInBlock($leaf, replacedText)
+          text = $textBlock.innerText
+        }
+      }
     }
   })
   $searchWindow.removeAttribute('loading')

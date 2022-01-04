b376ebac84133c972182f0d89ce5a1843fb3c6c5
yto60 <mail@yto60.dev>
2022-01-05 02:41:55 +0900

    update: add title

--- src/background.ts
+++ src/background.ts
@@ -7,6 +7,9 @@ import {
 const ACTIVE_ICON_COLOR = '#adadad'
 const INACTIVE_ICON_COLOR = '#d3d3d3'
 
+const ACTIVE_TITLE = 'Active (Click icon to hide search box.)'
+const INACTIVE_TITLE = 'Inactive (Click icon to show search box.)'
+
 async function main() {
   chrome.action.onClicked.addListener(async () => {
     const isActive = await getIsActive()
@@ -40,14 +43,19 @@ const getIconImageData = (color: string): ImageData => {
   return ctx.getImageData(0, 0, 16, 16)
 }
 
-const setIcon = (isActive: boolean) => {
+const setIconAndTitle = (isActive: boolean) => {
   const color = isActive ? ACTIVE_ICON_COLOR : INACTIVE_ICON_COLOR
   const imageData = getIconImageData(color)
   chrome.action.setIcon({ imageData })
+
+  const title = `Notion Replace Text:\n${
+    isActive ? ACTIVE_TITLE : INACTIVE_TITLE
+  }`
+  chrome.action.setTitle({ title })
 }
 
 const onIsActiveChange = (isActive: boolean) => {
-  setIcon(isActive)
+  setIconAndTitle(isActive)
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0]?.id!, { showSearchBox: isActive })
   })
